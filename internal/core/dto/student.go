c9697e3a09b0af28c1a8df7de054b8ef40f83151
JMjirapat <jirapat.prt@gmail.com>
2024-10-11 16:21:00 +0700

    add: omitempty in isTermAccepted

--- internal/core/dto/student.go
+++ internal/core/dto/student.go
@@ -2,5 +2,5 @@ package dto
 
 type StudentUpdateDto struct {
 	MajorID        int  `json:"major_id,omitempty"`
-	IsTermAccepted bool `json:"is_term_accepted"`
+	IsTermAccepted bool `json:"is_term_accepted,omitempty"`
 }
